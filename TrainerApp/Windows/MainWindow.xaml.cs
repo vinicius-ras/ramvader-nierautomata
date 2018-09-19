7d41582fd2dcaf0d3f069d3a4bcfc5e1ca95a5e6
Vinicius Rogério Araujo Silva <31182306+vinicius-ras@users.noreply.github.com>
2018-09-18 23:52:06 -0300

    * "God Mode" hack improved to prevent player from losing HP when hit by a red beam during the game's prologue (route A, while piloting a flight unit at the very beginning of the game).

--- TrainerApp/Windows/MainWindow.xaml.cs
+++ TrainerApp/Windows/MainWindow.xaml.cs
@@ -156,6 +156,7 @@ namespace TrainerApp
         {
             IntPtr mainModuleAddress = GameMemoryIO.GetTargetProcessModuleBaseAddress("NieRAutomata.exe");
             GameMemoryInjector.AddMemoryAlteration(ECheat.evCheatGodMode, new MemoryAlterationNOP(GameMemoryIO, new AbsoluteMemoryAddress(mainModuleAddress + 0x1FA423), 6));
+            GameMemoryInjector.AddMemoryAlteration(ECheat.evCheatGodMode, new MemoryAlterationNOP(GameMemoryIO, new AbsoluteMemoryAddress(mainModuleAddress + 0x1FA5B1), 6));
             GameMemoryInjector.AddMemoryAlteration(ECheat.evCheatInfiniteItems, new MemoryAlterationPoke(GameMemoryIO, new AbsoluteMemoryAddress(mainModuleAddress + 0x5DCFF6), new byte[] { 0xE9, 0xE2, 0xD1, 0x75, 0x00, 0x90, 0x90, 0x90 }));
             GameMemoryInjector.AddMemoryAlteration(ECheat.evCheatLevelUp, new MemoryAlterationPoke(GameMemoryIO, new AbsoluteMemoryAddress(mainModuleAddress + 0x5961AF), new byte[] { 0xE9, 0x41, 0x40, 0x7A, 0x00, 0x90 }));
         }
